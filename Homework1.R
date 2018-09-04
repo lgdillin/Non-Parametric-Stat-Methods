1353d63b8ac0333a13ec217a6fe295ca9898c065
lgdillin <mailgd9@gmail.com>
2018-09-04 18:49:21 -0500

    need help

--- Homework1.R
+++ Homework1.R
@@ -1,7 +1,7 @@
-# Liam Dillingham	
+# Liam Dillingham
 # 010732714
 
-
+### Problem 1
 # We can choose a head or a tail
 sample.space = c(0,1)
 
@@ -11,11 +11,18 @@ theta = 0.5
 # How many coin flips we want
 N = 100
 
+# Generate a dataset
 flips = sample(
 	sample.space,
 	size = N,
 	replace = TRUE,
 	prob = c(theta, 1 - theta)
-	)
+)
 
 flips
+
+
+### Problem 2
+rsample = sample(
+  
+)
