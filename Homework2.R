4851cdacdaa3e77c310234f5d3d213f057a4e11b
Liam Dillingham <mailgd9@gmail.com>
2018-09-21 14:43:38 -0500

    finish hw2

--- Homework2.R
+++ Homework2.R
@@ -7,18 +7,19 @@ age_group = c("18", "18", "19-20", "18", "18-19", "18-20", "18", "19", "18-19", 
 before_law = c(.262, .295, .216, .287, .227, .223, .512, .237, .348, .342)
 after_law = c(.202, .227, .191, .209, .299, .151, .471, .151, .336, .307)
 drinking = data.frame(state = state, age_group = age_group, before = before_law, after = after_law)
+boxplot(drinking$before, drinking$after)
 
 x = sum(drinking$after < drinking$before)
 n = length(drinking$before)
 
-binom.test(drinking$before, n, alternative = "l", conf.level = 0.95)
+model = binom.test(x, n, alternative = "g", conf.level = 0.95)
+cat("The probability of no effect is", model$p.value, "\n")
+cat("So reject Ho and accept Ha\n")
 
 # Wilcox test
-# Ho: The median change is >= 0
-wilcox.test(drinking$before, drinking$after, mu=0, alt="l", paired=T, conf.int = T, conf.level = 0.95, correct = F)
-
-
-wilcox.test
+model = wilcox.test(drinking$before, drinking$after, mu=0, alt="g", paired=T, conf.int = T, conf.level = 0.95, correct = F)
+cat("The probability of no effect is", model$p.value, "\n")
+cat("So reject Ho and accept Ha\n")
 
 ### Problem 2
 student = c(toupper(letters[1:14]))
@@ -27,8 +28,18 @@ after_course = c(20, 18, 10, 16, 11, 20, 20, 19, 13, 21, 12, 15, 14, 17)
 computer_anxiety = data.frame(student = student, before = before_course, after = after_course)
 computer_anxiety
 
+x = sum(computer_anxiety$after < computer_anxiety$before)
+n = length(computer_anxiety$before)
+
 # Ho: The median change is >= 0
-wilcox.test(computer_anxiety$before, computer_anxiety$after, mu=0, alt="l", paired=T, conf.int = T, conf.level = 0.95, correct = F)
+model = binom.test(x, n, alternative = "g", conf.level = 0.95)
+cat("The probability of no effect is", model$p.value, "\n")
+cat("So reject Ho and accept Ha\n")
+
+# Ho: The median change is >= 0
+model = wilcox.test(computer_anxiety$before, computer_anxiety$after, mu=0, alt="g", paired=T, conf.int = T, conf.level = 0.95, correct = F)
+cat("The probability of no effect is", model$p.value, "\n")
+cat("So reject Ho and accept Ha\n")
 
 ### Problem 3
 
@@ -49,3 +60,26 @@ qqnorm(log_audio_data)
 ## The log transformed data appears more normal
 
 ### Bonus question
+set.seed(123)
+
+func_ = function(a, b, n=1) {
+  # Signature for runif(n,high,low)
+  u = runif(n)
+  #if(u >= 0) {
+    x = (a * b^a) / u^(a+1) 
+  #} else {
+  #  x = 0
+  #}
+  return(x)
+}
+
+a = 5
+b = 2
+n = 1000
+x = func_(a, b, n)
+hist(x, breaks=30, freq=F)
+
+
+
+
+
