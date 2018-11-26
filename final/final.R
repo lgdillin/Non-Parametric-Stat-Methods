777a444c56642a0b6ab883cbaa8be996c3278aeb
Liam Dillingham <mailgd9@gmail.com>
2018-11-26 15:58:37 -0600

    some work

--- final/final.R
+++ final/final.R
@@ -10,13 +10,25 @@ load(url("http://statweb.stanford.edu/~ckirby/brad/LSI/datasets-and-programs/dat
 dim(leukdata)
 
 # Obtaining R packages:
+# for (base) biocLite and (dependent) multtest:
+source("http://bioconductor.org/biocLite.R")
+biocLite("Biobase")
+biocLite(c("multtest","preprocessCore"))
 
-# try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-biocLite("ALL")
+# verifies that multtest has been installed correctly
+# if running the below command prints nothing, you are good to go.
+library(multtest)
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("multtest")
+levels = c(rep(1, 50), rep(2, 52))
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("limma")
+i = 1
+for(i in nrow(prostatedata)) {
+  prow = prostatedata[i,]
+  
+}
+
+lev = prostatedata[0,]
+row = prostatedata[1,]
+kruskal.test(row ~ lev)
+
+
--- final/finalExample.R
+++ final/finalExample.R
@@ -30,3 +30,39 @@ head(mydata, n = 3)
 
 eset <- ALL[, ALL$mol.biol %in% c("BCR/ABL", "ALL1/AF4")]
 dim(eset)
+
+require(multtest)
+
+## loading required package: multtest
+all.mat = exprs(eset)
+all.cl = factor(as.character(eset$mol.biol))
+teststat = mt.teststat(all.mat, all.cl, test = "t")
+
+# the two datasets all.mat and all.cl are all we need
+# all.mat has the gene expression values and all.cl gives class labels
+# mt.teststat {multtest}: package for computing test stats for each row of a data frame
+
+
+## We need to manage the multiplicity of error when we conduct multiple tests
+# we can divide our significance leve (0.05) by the number of tests to produce a better control
+# family wise error rate (FWER): P(at least 1 false rejection) <= 0.05
+
+rawp = 2 * (1 - pnorm(abs(teststat)))
+selected = p.adjust(rawp, method = "bonferroni") < 0.05
+esetSel = eset[selected,]
+sum(selected)
+
+rawp = 2 * (1 - pnorm(abs(teststat)))
+selected = p.adjust(rawp, method = "BH") < 0.05
+esetSel = eset[selected,]
+sum(selected)
+
+# Non parametric
+dim(all.mat)
+dim(all.cl)
+str(all.cl)
+
+all.mat.af4 = all.mat[ ,all.cl == 'ALL1/AF4']
+all.mat.ab1 = all.mat[ ,all.cl == 'BCR/ABL']
+
+wilcox.test(all.mat.af4[1,], all.mat.ab1[1,])
