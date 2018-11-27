ad1068298df6a55472b637ade86bfd4cb175f3d9
Liam Dillingham <mailgd9@gmail.com>
2018-11-27 12:20:21 -0600

    some work

--- final/final.R
+++ final/final.R
@@ -19,16 +19,30 @@ biocLite(c("multtest","preprocessCore"))
 # if running the below command prints nothing, you are good to go.
 library(multtest)
 
+#######################################
+##### PROBLEM #1: PROSTATE DATA #######
+#######################################
+
+# For ease of use, I simply created my own group vector b/c I couldn't figure out how to parse the RData one
 levels = c(rep(1, 50), rep(2, 52))
 
-i = 1
-for(i in nrow(prostatedata)) {
+normal = 0
+notnormal = 0
+# For every row in our dataset
+for(i in 1:nrow(prostatedata)) {
   prow = prostatedata[i,]
   
+  # Test if the gene is normally distributed between normal and cancer patients
+  normy = shapiro.test(prow)$p.value
+  
+  # If it is, we can perform a t-test on the row (b)
+  # Otherwise, use a non-parametric test
+  if(normy < 0.05) {
+    notnormal = notnormal + 1
+  } else {
+    normal = normal + 1
+    t.test()
+  }
 }
-
-lev = prostatedata[0,]
-row = prostatedata[1,]
-kruskal.test(row ~ lev)
 
 
